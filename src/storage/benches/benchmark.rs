cb3135ff5b6f0156f32d9bfdd8c8c5403ffb61c0
Poovamraj Thanganadar Thiagarajan <poovamraj@gmail.com>
2024-10-13 16:56:20 +0200

    Implement benchmarks

--- src/storage/benches/benchmark.rs
+++ src/storage/benches/benchmark.rs
@@ -1,10 +1,11 @@
 use std::fs;
 use std::path::Path;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use heed::EnvOpenOptions;
 use storage::new_heed;
 use storage::Storage;
 
-fn main() {
+pub fn criterion_benchmark(c: &mut Criterion) {
     let path = Path::new("target").join("heed.mdb");
     fs::create_dir_all(&path).expect("failure creating dir");
     let env = unsafe { EnvOpenOptions::new().open(&path) }.expect("failure with env");
@@ -12,8 +13,11 @@ fn main() {
     let wtxn = env.write_txn().expect("failure with write"); // We open the default unnamed database
     let mut heed = new_heed(env.clone(), wtxn).expect("Cannot create");
 
-    heed.create_workflow("test").expect("Cannot Write");
-    let result = heed.get_workflow("test").expect("Cannot Read");
-    println!("{:?}", result);
+    c.bench_function("benchmark running heed storage write and get", |b| b.iter(|| {
+        heed.create_workflow(black_box("test")).expect("Cannot Write");
+        heed.get_workflow(black_box("test")).expect("Cannot Read");
+    }));
 }
 
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
